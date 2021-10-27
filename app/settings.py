989f04cbfdc4367c32b6abd1d846db09dca210f8
tsepanx <77626319+tsepanx@users.noreply.github.com>
2021-10-27 22:52:28 +0300

    CustomUser model

--- app/settings.py
+++ app/settings.py
@@ -15,6 +15,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'blog.CustomUser'
+
 
 # Application definition
 
@@ -25,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from app import settings
+
+
+class CustomUser(AbstractUser):
+    pass
+
+
+class Article(models.Model):
+    # author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        # django.contrib.auth.get_user_model()
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    title = models.CharField(max_length=200)
+
+    pub_date = models.DateTimeField('Date published')
+    text = models.CharField(max_length=10 ** 4)
